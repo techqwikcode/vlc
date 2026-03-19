d1d4e0f33a61ce98cf0291677d4b06c8a332ac36
Tech Qwik Code <educationmail02@gmail.com>
2026-03-19 21:59:31 +0530

    android folder added

---
+++ android/app/src/main/java/com/srk/mediaplayer/MainActivity.java
@@ -0,0 +1,5 @@
+package com.srk.mediaplayer;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.srk.mediaplayer',
+  appName: 'srk_media_player',
+  webDir: 'dist'
+};
+
+export default config;
